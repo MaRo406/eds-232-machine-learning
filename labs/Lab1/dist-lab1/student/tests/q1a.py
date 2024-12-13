109e462f28e6fc3213d5668327bcea4f5bf07c4c
annieradams <adams.annie11@gmail.com>
2024-12-13 11:36:37 -0800

    Init otter grader questions

---
+++ labs/Lab1/dist-lab1/student/tests/q1a.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1a',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': '>>> assert X_train.index[0] == np.int64(249)\n>>> assert len(X_train) == 771\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
